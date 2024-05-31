cacbfaa0fcd7af43e19457a45c72b95323b25980
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-05-31 08:23:23 -0300

    try-catch-finalizado

--- full-stack-dev-puc-minas/15-try-catch/script.js
+++ full-stack-dev-puc-minas/15-try-catch/script.js
@@ -1,4 +1,18 @@
-xyz.innerHTML = '';
+output.innerHTML = '';
 
-let a = x;
-console.log('Fim')
+let a;
+let x = -10;
+try {
+    if (x < 0)
+        throw new TypeError("Valor não pode ser negativo");
+    a = x;
+    output.innerHTML = `<p>A = </br>${a}</p>`;
+
+} catch (erro) {
+    output.innerHTML = `<p>${erro.message}</p>`;
+    console.log(erro.name);
+    console.log(erro.message);
+    console.log(erro.stack);
+} finally {
+    console.log('Fim');
+}
