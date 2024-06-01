72c59bfe6a2f621644bd5afb1f947bccc573bc07
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-06-01 01:05:42 -0300

    console-messages-types

---
+++ full-stack-dev-puc-minas/javascript/25-console/exemplo1.js
@@ -0,0 +1,20 @@
+(function mensagens() {
+    console.log('mensagem');
+    console.info('mensagem');
+    console.debug('mensagem');
+    console.warn('mensagem');
+    console.error('mensagem');
+})();
+
+console.log('%cOlá %s. Hoje é dia %d', 'color: red; font-size: larger', 'Marcos', 30)
+
+console.table([1, 2, 3]);
+console.table([
+    {nome: 'José', idade: 35},
+    {apelido: 'Maria', prazo: 28}
+])
+
+console.count('a');
+console.count('a');
+console.count('a');
+
