6e9db148168b5cc0db0372eb732538cacaba6043
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-06-11 16:42:15 -0300

    Refazendo-curso-c

---
+++ udemy/1-aprenda-e-domine-a-linguagem-c/1-primeiro-programa/primeiro-programa.c
@@ -0,0 +1,11 @@
+// Primeiro Programa.cpp
+// Autor: Jefferson Dutra
+// Objetivo: Apresentar o primeiro programa em Linguagem C
+
+#include <stdio.h> // Include the standard input/output library
+
+int main() // The main function
+{
+    printf("Meu primeiro programa!"); // Print a message
+    return 0; // Return 0 to indicate that the program executed successfully
+}
---
+++ udemy/1-aprenda-e-domine-a-linguagem-c/2-ex-apresenta-info/ex-apresenta-info.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // Include the standard input/output library
+
+int main() // The main function
+{
+    printf("Jefferson Dutra \n");
+    printf("O nome da minha mamae \n");
+    printf("O nome da minha vovo \n");
+    return 0;
+}
