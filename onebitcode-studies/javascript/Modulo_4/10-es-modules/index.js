bbbd1d9f6ac73899b9042103704225c4d88db98a
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-07-25 01:03:46 -0300

    es-modules

---
+++ onebitcode-studies/javascript/Modulo_4/10-es-modules/index.js
@@ -0,0 +1,6 @@
+// Palavra import reservada para importação de modulos
+import { label, input, br } from './functions.js'
+
+console.log(label({ for: 'fullname', textContent: 'Nome Completo'}))
+console.log(input({ id: 'fullname', name: 'fullname', placeholder: 'Digite seu nome completo...'}))
+console.log(br())
