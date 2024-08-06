2287363c3973eadb8c48a414f11548030c3b18ee
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-08-06 14:59:23 -0300

    api-restfull-finalizada

--- full-stack-dev-puc-minas/nodejs/2-apis/projeto-api/routes/apiRouterV1.js
+++ full-stack-dev-puc-minas/nodejs/2-apis/projeto-api/routes/apiRouterV1.js
@@ -18,6 +18,7 @@ apiRouterV1.get('/produtos', function(req, res, next) {
   res.json(produtos)
 });
 
+// cRud --> Read
 apiRouterV1.get('/produtos/:id', function(req, res, next) {
   let id = req.params.id;
   if (id) {
@@ -25,6 +26,56 @@ apiRouterV1.get('/produtos/:id', function(req, res, next) {
     let idx = produtos.findIndex(o => o.id === idInt)
     if (idx > -1) {
       res.json(produtos[idx])
+    }
+    else {
+      res.status(404).json({ message: `Produto não encontrado`})
+    }
+  } 
+  else {
+    res.status(404).json({ message: `Produto não encontrado`})
+  }
+});
+
+// Crud --> Create
+apiRouterV1.post('/produtos', function(req, res, next) {
+  let produto = req.body
+  let newId = Math.max(...produtos.map(o => o.id)) + 1
+  produto.id = newId
+  produtos.push (produto)
+  res.status(201).json({ message: `Produto inserido com sucesso`, data: { id: newId }})
+});
+
+// cruD --> Delete
+apiRouterV1.delete('/produtos/:id', function(req, res, next) {
+  let id = req.params.id;
+  if (id) {
+    idInt = Number.parseInt(id)
+    let idx = produtos.findIndex(o => o.id === idInt)
+    if (idx > -1) {
+      produtos.splice(idx, 1)
+      res.status(200).json({ message: `Produto excluido com sucesso`})
+    }
+    else {
+      res.status(404).json({ message: `Produto não encontrado`})
+    }
+  } 
+  else {
+    res.status(404).json({ message: `Produto não encontrado`})
+  }
+});
+
+// crUd --> Update
+apiRouterV1.put('/produtos/:id', function(req, res, next) {
+  let id = req.params.id;
+  produto = req.body
+  if (id) {
+    idInt = Number.parseInt(id)
+    let idx = produtos.findIndex(o => o.id === idInt)
+    if (idx > -1) {
+      produtos[idx].descricao = produto.descricao
+      produtos[idx].marca = produto.marca
+      produtos[idx].preco = produto.preco
+      res.status(200).json({ message: `Produto alterado com sucesso`, data: { produto: produtos[idx] }})
     }
     else {
       res.status(404).json({ message: `Produto não encontrado`})
