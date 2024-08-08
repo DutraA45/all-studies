7d3a8688e60566281f5d3a95762dd27a32d6c0f0
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-08-08 16:56:20 -0300

    api-finalizada

--- full-stack-dev-puc-minas/nodejs/2-apis/projeto-api/routes/apiRouterV2.js
+++ full-stack-dev-puc-minas/nodejs/2-apis/projeto-api/routes/apiRouterV2.js
@@ -1,82 +1,91 @@
 var express = require('express');
 var apiRouterV2 = express.Router();
 
-const knex = require ('knex')(require('../knexfile').development)
+const knex = require('knex')(require('../knexfile').development)
 
-apiRouterV2.get('/produtos', function(req, res, next) {
+apiRouterV2.get('/produtos', function (req, res, next) {
   knex('produtos')
-  .select('*')
-  .then(produtos => {
-    res.status(200).json(produtos)
-  })
-  .catch(err => res.status(500).json({ message: `Erro ao obter produtos: ${err.message}`}))
+    .select('*')
+    .then(produtos => {
+      res.status(200).json(produtos)
+    })
+    .catch(err => res.status(500).json({ message: `Erro ao obter produtos: ${err.message}` }))
 });
 
 // cRud --> Read
-apiRouterV2.get('/produtos/:id', function(req, res, next) {
+apiRouterV2.get('/produtos/:id', function (req, res, next) {
   let id = req.params.id;
   if (id) {
     idInt = Number.parseInt(id)
-    let idx = produtos.findIndex(o => o.id === idInt)
-    if (idx > -1) {
-      res.json(produtos[idx])
-    }
-    else {
-      res.status(404).json({ message: `Produto não encontrado`})
-    }
-  } 
-  else {
-    res.status(404).json({ message: `Produto não encontrado`})
+    knex('produtos')
+      .select('*')
+      .where({ id: idInt })
+      .then(produtos => {
+        if (!produtos.length) {
+          res.status(404).json({ message: `Produto não encontrado` })
+          return
+        }
+        let produto = produtos[0]
+        res.status(200).json(produto)
+      })
+      .catch(err => res.status(500).json({ message: `Erro ao obter produtos: ${err.message}` }))
   }
+  else
+    res.status(404).json({ message: `Produto não encontrado` })
 });
 
 // Crud --> Create
-apiRouterV2.post('/produtos', function(req, res, next) {
+apiRouterV2.post('/produtos', function (req, res, next) {
   let produto = req.body
-  let newId = Math.max(...produtos.map(o => o.id)) + 1
-  produto.id = newId
-  produtos.push (produto)
-  res.status(201).json({ message: `Produto inserido com sucesso`, data: { id: newId }})
+  knex('produtos')
+    .insert(produto, ['id'])
+    .then(produtos => {
+      if (!produtos.length) {
+        res.status(400).json({ message: `Erro ao inserir produto` })
+        return
+      }
+      else {
+        let id = produtos[0].id
+        res.status(201).json({ message: `Produto inserido com sucesso`, data: { id } })
+      }
+    })
+    .catch(err => res.status(500).json({ message: `Erro ao inserir produto: ${err.message}` }))
 });
 
 // cruD --> Delete
-apiRouterV2.delete('/produtos/:id', function(req, res, next) {
+apiRouterV2.delete('/produtos/:id', function (req, res, next) {
   let id = req.params.id;
   if (id) {
     idInt = Number.parseInt(id)
-    let idx = produtos.findIndex(o => o.id === idInt)
-    if (idx > -1) {
-      produtos.splice(idx, 1)
-      res.status(200).json({ message: `Produto excluido com sucesso`})
-    }
-    else {
-      res.status(404).json({ message: `Produto não encontrado`})
-    }
-  } 
+    knex('produtos')
+      .where({ id: idInt })
+      .del()
+      .then(result => {
+        res.status(200).json({ message: `Produto excluído com sucesso` })
+      })
+      .catch(err => res.status(500).json({ message: `Erro ao excluir o produto: ${err.message}` }))
+  }
   else {
-    res.status(404).json({ message: `Produto não encontrado`})
+    res.status(404).json({ message: `Produto não encontrado` })
   }
 });
 
 // crUd --> Update
-apiRouterV2.put('/produtos/:id', function(req, res, next) {
+apiRouterV2.put('/produtos/:id', function (req, res, next) {
   let id = req.params.id;
   produto = req.body
   if (id) {
     idInt = Number.parseInt(id)
-    let idx = produtos.findIndex(o => o.id === idInt)
-    if (idx > -1) {
-      produtos[idx].descricao = produto.descricao
-      produtos[idx].marca = produto.marca
-      produtos[idx].preco = produto.preco
-      res.status(200).json({ message: `Produto alterado com sucesso`, data: { produto: produtos[idx] }})
-    }
-    else {
-      res.status(404).json({ message: `Produto não encontrado`})
-    }
-  } 
+    knex('produtos')
+    .where({ id: idInt })
+    .update(produto)
+    .then(result => {
+      res.status(200).json({ message: `Produto alterado com sucesso`, data: { produto } })
+    })
+    .catch(err => res.status(500).json({ message: `Erro ao atualizar o produto: ${err.message}` }))
+  }
   else {
-    res.status(404).json({ message: `Produto não encontrado`})
+    res.status(404).json({ message: `Produto não encontrado` })
   }
 });
 
