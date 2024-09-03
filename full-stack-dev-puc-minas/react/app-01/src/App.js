b3eaf05cfba4e2e942de104713eb660d76f4fdd8
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-09-03 00:14:47 -0300

    inicio-todo-list-react

--- full-stack-dev-puc-minas/react/app-01/src/App.js
+++ full-stack-dev-puc-minas/react/app-01/src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import { useEffect, useState } from 'react';
 import './App.css';
-import { Header } from './components/Header';
-import { SkillsList } from './components/SkillsList';
 
 
 function App() {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    console.log("renderizou")
-  }, [loading])
-
   return (
-    <div className="App">
-      <Header firstName={"Jefferson"} />
-      <SkillsList skills={["css", "html", "javascript", "react"]} />
-
-      <button onClick={() => setLoading(!loading)}>{loading ? "Loading ..." : "Salvar"}</button>
-    </div>
+    <></>
   );
 }
 
