d331bd513f59f50f05b71953ce240308d157e9e5
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-10-01 15:26:03 -0300

    att-api-key-security

--- full-stack-dev-puc-minas/react/movies-spa/src/config/http.js
+++ full-stack-dev-puc-minas/react/movies-spa/src/config/http.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const moviesApi = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZjY3MzcwMTEzNTIzZWQwODQyMzQzMWU5YmY3MjdmZiIsIm5iZiI6MTcyNjE2OTYzOS4yOTU4NTcsInN1YiI6IjY2ZGEwYzJlYWU0NGViNjE5NjYwNjlkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qvJQ4aNmeS0h-G_N7WsU_5MtcfI8Kc5mbvAICido0nw`
+        Authorization: `Bearer ${process.env.MOVIE_DB_API_KEY}`
     }
 });
