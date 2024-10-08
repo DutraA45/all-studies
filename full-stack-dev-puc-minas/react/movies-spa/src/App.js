bba4a8c84243079aa2e5c375ca0aff9b3e3fdd75
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-10-08 01:03:02 -0300

    config-inicial-redux

--- full-stack-dev-puc-minas/react/movies-spa/src/App.js
+++ full-stack-dev-puc-minas/react/movies-spa/src/App.js
@@ -1,9 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
 import { routes } from './views/routes';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <RouterProvider router={routes} />
+    <Provider store={store}>
+      <RouterProvider router={routes} />
+    </Provider>
   );
 }
 
