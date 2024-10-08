06554f0b792a9c74310370b9edd3dec1e2e3959c
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-10-08 01:46:22 -0300

    add-favoritoButton

--- full-stack-dev-puc-minas/react/movies-spa/src/components/movieItem/MovieItem.js
+++ full-stack-dev-puc-minas/react/movies-spa/src/components/movieItem/MovieItem.js
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 import styles from "./MovieItem.module.css";
+import { useDispatch } from "react-redux";
+import { addMovie } from "../../store/reducers/favorito"
+
 export function MovieItem({ movie }) {
+    const dispatch = useDispatch();
+    
     return (
         <div className={styles.movie}>
             {movie.poster_path && (
@@ -9,6 +14,7 @@ export function MovieItem({ movie }) {
             <h3>{movie.title}</h3>
             <p>
                 <Link className={styles.detailsBtn} to={`/movies/${movie.id}`}>Ver detalhes</Link>
+                <button onClick={() => dispatch(addMovie(movie))}>Adicionar aos favoritos</button>
             </p>
         </div>
     )
