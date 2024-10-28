8b8c11ee17e7d246b6891ba4471175324c4f3a2e
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-10-28 11:49:56 -0300

    lazy-loading-reactjs

--- full-stack-dev-puc-minas/react/movies-spa/src/views/routes.js
+++ full-stack-dev-puc-minas/react/movies-spa/src/views/routes.js
@@ -1,26 +1,41 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { Home } from "./Home";
-import { Movies } from "./movies/Movies";
 import { RootLayout } from "./RootLayout";
-import { MovieDetail } from './movieDetail/MovieDetail';
 import { Favorites } from './favorites/Favorites';
 
 export const routes = createBrowserRouter([
     {
         path: "/",
-        element: <RootLayout />, 
+        element: <RootLayout />,
         children: [
             {
                 path: "/",
-                element: <Home />
+                lazy: async () => {
+                    const { Home } = await import("./views/Home")
+
+                    return {
+                        Component: Home
+                    }
+                }
             },
             {
                 path: "/movies",
-                element: <Movies />
+                lazy: async () => {
+                    const { Movies } = await import("./movies/Movies")
+
+                    return {
+                        Component: Movies
+                    }
+                }
             },
             {
                 path: "/movies/:id",
-                element: <MovieDetail />
+                lazy: async () => {
+                    const { MovieDetail } = await import("./movieDetail/MovieDetail")
+
+                    return {
+                        Component: MovieDetail
+                    }
+                }
             },
             {
                 path: "/favorites",
