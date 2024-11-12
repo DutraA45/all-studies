72da892b04434b6d5e8fbe4648257b9cf941213a
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-11-12 19:54:10 -0300

    implementacao-database

--- full-stack-dev-puc-minas/5-apis-webservices/APIs_Class/apiContatos/src/api-v1/paths/pessoa-router.js
+++ full-stack-dev-puc-minas/5-apis-webservices/APIs_Class/apiContatos/src/api-v1/paths/pessoa-router.js
@@ -3,13 +3,28 @@ import pessoaModel from '../models/pessoa-model';
 
 const pessoaRouter = express.Router(); 
 
-// pessoaRouter.use('/', (req, res, next) => res.send('ENDPOINT PESSOA'));
 pessoaRouter.get('/', listaPessoas);
+pessoaRouter.post('/', inserePessoas);
+
+pessoaRouter.put('/', inserePessoas);
+pessoaRouter.delete('/', inserePessoas);
+
+pessoaRouter.get('/{id}', inserePessoas);
 
 function listaPessoas(req, res, next) {
     pessoaModel.lista({}, (err, lista) => {
         if (!err) {
             res.json(lista);
+        } else {
+            res.status(400).send(err.message)
+        }
+    })
+}
+
+function inserePessoas(req, res, next) {
+    pessoaModel.insere(req.body, (err, objNovo) => {
+        if (!err) {
+            res.json(objNovo);
         } else {
             res.status(400).send(err.message)
         }
--- full-stack-dev-puc-minas/5-apis-webservices/APIs_Class/apiContatos/src/server.js
+++ full-stack-dev-puc-minas/5-apis-webservices/APIs_Class/apiContatos/src/server.js
@@ -1,8 +1,17 @@
 import http from 'http';
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import apiRouter from './api-v1/api-router';
 
 const app = express();
+
+// associar o json ao objeto req.body
+app.use (bodyParser.json());
+
+// associa os parametros de URL e Body com fformato urlEncoded
+// ao objeto req.params
+app.use (bodyParser.urlencoded( { extended: false } )); 
 
 app.use ('/api/v1', apiRouter );
 app.use ('/', (req, res) => res.send ('-- API Contatos --') );
