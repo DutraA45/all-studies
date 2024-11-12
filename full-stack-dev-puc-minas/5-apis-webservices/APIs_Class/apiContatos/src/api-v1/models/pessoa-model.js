8fa48bdb81ce52e5f0f9fe979dba2c0cd2b28586
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-11-12 15:35:20 -0300

    get-lista-implementado

---
+++ full-stack-dev-puc-minas/5-apis-webservices/APIs_Class/apiContatos/src/api-v1/models/pessoa-model.js
@@ -0,0 +1,39 @@
+
+
+function insere() {
+
+}
+
+function lista(params, callback) {
+    const objFake = [
+        {
+            "id": 5,
+            "nome": "Francis Ford Copolla",
+            "email": "copolla@hollywood.com",
+            "telefone": "001-124-2111",
+            "organizacao": null,
+            "tags": [
+                "cinema",
+                "trabalho"
+            ]
+        }
+    ];
+
+    const err = null
+    callback (err, objFake)
+}
+
+function altera() {
+
+}
+
+function exclui() {
+
+}
+
+export default {
+    insere,
+    lista,
+    altera,
+    exclui
+}
