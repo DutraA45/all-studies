f816bba439e838d1b2a6f045b637c1fba183e338
Jefferson Dutra <jeffinho.angelo@gmail.com>
2024-11-12 15:19:48 -0300

    roteamento

--- full-stack-dev-puc-minas/5-apis-webservices/APIs_Class/apiContatos/src/server.js
+++ full-stack-dev-puc-minas/5-apis-webservices/APIs_Class/apiContatos/src/server.js
@@ -1,9 +1,11 @@
 import http from 'http';
 import express from 'express';
+import apiRouter from './api-v1/api-router';
 
 const app = express();
 
-app.all ('/', (req, res, next) => res.send ('Tudo funcionando!') );
+app.use ('/api/v1', apiRouter );
+app.use ('/', (req, res) => res.send ('-- API Contatos --') );
 
 const porta = 5500;
 http.createServer(app).listen(porta, () => 
