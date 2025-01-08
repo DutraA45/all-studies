4b7c1809ce2b74c2a219e1596062a3574b085bfa
Jefferson Dutra <jeffinho.angelo@gmail.com>
2025-01-08 16:37:55 -0300

    dart-aula-1

---
+++ youtube/flutter/introducao-dart/1-variaveis.dart
@@ -0,0 +1,14 @@
+// 1 - Introdução a Variaveis
+
+void main() {
+  // Tipos
+  String variavelNome = "Jefferson";
+  int valorVariavel = 10;
+  bool ehVerdadeiro = true;
+  List<String> listaDePalavras = ["Jefferson", "Dutra"];
+
+  print(variavelNome);
+  print(valorVariavel);
+  print(ehVerdadeiro);
+  print('${listaDePalavras[0]} - ${listaDePalavras[1]}');
+}
