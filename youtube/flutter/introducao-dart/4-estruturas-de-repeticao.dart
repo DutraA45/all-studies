9354935bf265a86bc0a2e34a1de84c243284f9a5
Jefferson Dutra <jeffinho.angelo@gmail.com>
2025-01-08 16:38:14 -0300

    dart-aula-4

---
+++ youtube/flutter/introducao-dart/4-estruturas-de-repeticao.dart
@@ -0,0 +1,15 @@
+// 4 - Estruturas de Repeticao
+
+void main() {
+  // Exemplo For
+  for (int i = 1; i <= 10; i++) {
+    print(i * 2);
+  }
+
+  // Exemplo While
+  int contador = 10;
+  while (contador != 1) {
+    contador = contador - 1;
+    print('Loop -> $contador');
+  }
+}
