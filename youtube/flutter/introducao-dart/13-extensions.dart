1f4a52ab711ec129ba1e6398de06ee027b2efefb
Jefferson Dutra <jeffinho.angelo@gmail.com>
2025-01-09 22:15:03 -0300

    dart-aula-13

---
+++ youtube/flutter/introducao-dart/13-extensions.dart
@@ -0,0 +1,31 @@
+// 1 - Introdução a Variaveis
+
+void main() {
+  String nome = "jefferson";
+  print(Utils().toFirstCharToUpperCase(nome));
+  print(nome.toFirstCharToUpperCase());
+  print(EnumTest.enumValue.toValue());
+}
+
+enum EnumTest {
+  enumValue;
+}
+
+extension ExtensionsEnum on Enum {
+  String toValue() {
+    Map map = {EnumTest.enumValue: "xpto"};
+    return map[this];
+  }
+}
+
+extension ExtensionString on String {
+  String toFirstCharToUpperCase() {
+    return this[0].toUpperCase() + this.substring(1);
+  }
+}
+
+class Utils {
+  toFirstCharToUpperCase(String value) {
+    return value[0].toUpperCase() + value.substring(1);
+  }
+}
