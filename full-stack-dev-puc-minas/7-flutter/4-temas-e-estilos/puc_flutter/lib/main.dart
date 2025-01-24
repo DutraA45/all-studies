4f2e78a6e2708e839a3415579e0bcdd3ddff6775
Jefferson Dutra <jeffinho.angelo@gmail.com>
2025-01-24 15:31:40 -0300

    aula-temas-dart

--- full-stack-dev-puc-minas/7-flutter/4-temas-e-estilos/puc_flutter/lib/main.dart
+++ full-stack-dev-puc-minas/7-flutter/4-temas-e-estilos/puc_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:puc_flutter/search.dart';
 
 void main() {
@@ -11,8 +12,16 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromARGB(255, 40, 150, 115)),
+          textTheme: GoogleFonts.acmeTextTheme()),
       home: Scaffold(
-          appBar: AppBar(title: const Text('Meu primeiro app em flutter')),
+          appBar: AppBar(
+              title: const Text(
+            'Meu primeiro app em flutter',
+            style: TextStyle(fontSize: 48, fontStyle: FontStyle.italic),
+          )),
           bottomNavigationBar: BottomNavigationBar(items: const [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
             BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Busca'),
