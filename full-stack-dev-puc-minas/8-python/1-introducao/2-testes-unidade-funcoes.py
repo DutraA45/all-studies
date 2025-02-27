c9e61ce86892ef2d70ee4cca7ab35a720f2739f0
Jefferson <jeffinho.angelo@gmail.com>
2025-02-27 15:13:25 -0300

    exercicio-2

---
+++ full-stack-dev-puc-minas/8-python/1-introducao/2-testes-unidade-funcoes.py
@@ -0,0 +1,21 @@
+# Desafio 2
+
+
+# João adora andar de bicicleta. Ele sabe que é importante se manter hidratado, por
+# isso ele bebe 0.5 litro de água por hora de ciclismo. Escreva uma função em Python,
+# e o teste de unidade associado, que receba o tempo em horas e retorne a
+# quantidade de litros que João beberá, arredondado para o menor valor.
+
+# Exemplos:
+# Entradas --> Saída:
+# tempo = 3 --> litros = 1
+# tempo = 6.7 --> litros = 3
+# tempo = 11.8 --> litros = 5
+
+from math import floor
+
+def hidratacao(tempo):
+    agua = floor(tempo/2)
+    return print("litros = ", agua)
+
+hidratacao(11.8)
