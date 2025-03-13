edd371278c1a47d6fc3d2efeda1ec55254a42b75
Jefferson <jeffinho.angelo@gmail.com>
2025-03-13 15:02:40 -0300

    exercicio-10

--- full-stack-dev-puc-minas/8-python/1-introducao/10-exercicio-poo.py
+++ full-stack-dev-puc-minas/8-python/1-introducao/10-exercicio-poo.py
@@ -1,3 +1,18 @@
+# Desafio 14.1
+
+# Escreva uma classe em Python chamada Musica. Uma nova música terá dois parâmetros,
+# título e artista.
+
+# que_pais_e_este => Musica("Que Pais é Este", "Legião Urbana")
+# que_pais_e_este.titulo => "Que Pais é Este"
+# que_pais_e_este.artista => "Legião Urbana"
+
+# Você também terá que criar um método de instância chamado
+# conta_numero_de_ouvintes(). O método leva uma série de pessoas que ouviram a
+# música naquele dia. A saída deve ser quantos novos ouvintes a música ganhou naquele dia de
+# todos os ouvintes. Os nomes devem ser tratados sem distinção entre maiúsculas e minúsculas,
+# ou seja, "JoHn" é o mesmo que "john".
+
 class Musica:
     def __init__(self, titulo, artista):
         self.titulo = titulo
